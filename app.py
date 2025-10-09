fbf8d142906e34c31b41e044ae19e1ae7a7d8362
PrimeFox59 <primetroyxs@gmail.com>
2025-10-09 07:58:57 +0700

    1

--- app.py
+++ app.py
@@ -1069,16 +1069,16 @@ def main():
     st.sidebar.image("logo.png", use_container_width=True)
     st.sidebar.title("Navigasi")
 
-    if not user and st.session_state.page != 'RestoreStatus':
-        if st.sidebar.button("🔐 Login / Register", use_container_width=True):
-            st.session_state.page = "Authentication"
-    else:
+    if user:
         # Info singkat user
         st.sidebar.markdown(f"**👤 {user['name']}**")
         st.sidebar.markdown(f"✉️ {user['email']}")
         st.sidebar.markdown(f"🏢 {user.get('department','-')}")
         st.sidebar.markdown(f"**Role:** {user['role'].capitalize()}")
         st.sidebar.markdown("---")
+    elif st.session_state.page != 'RestoreStatus':
+        if st.sidebar.button("🔐 Login / Register", use_container_width=True):
+            st.session_state.page = "Authentication"
 
 
         # --- Improved: Guarantee Auto-Restore before Auto-Backup ---
